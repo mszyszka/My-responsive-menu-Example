bc57f5e16b6585d847186e649042219df4785658
Michał Szyszka <mszyszka223@gmail.com>
2017-09-14 11:06:49 +0200

    menu fot mobile is ready

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function hamburgerBtn(x) {
     document.getElementsByClassName("page-nav")[0].classList.toggle("nav-opened");
 }
 
+function openSubmenuGrapes(x) {
+    document.getElementsByClassName("grapes-ul")[0].classList.toggle("submenu-opened");
+}
+
+function openSubmenuApples(x) {
+    document.getElementsByClassName("apples-ul")[0].classList.toggle("submenu-opened");
+}
+function openSubmenuOranges(x) {
+    document.getElementsByClassName("oranges-ul")[0].classList.toggle("submenu-opened");
+}
+function openSubmenuPineapple(x) {
+    document.getElementsByClassName("pineapple-ul")[0].classList.toggle("submenu-opened");
+}
+function openSubmenuBananas(x) {
+    document.getElementsByClassName("bananas-ul")[0].classList.toggle("submenu-opened");
+}
+

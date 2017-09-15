fc773798f11e04688fa460cc51026c2a67137c87
Michał Szyszka <mszyszka223@gmail.com>
2017-09-15 16:40:20 +0200

    I'm trying to make my js shorter

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function hamburgerBtn(x) {
     x.classList.toggle("change");
     document.getElementsByClassName("page-nav")[0].classList.toggle("nav-opened");
 }
+var subMenuNames = ["grapes-ul", "apples-ul", "oranges-ul", "pineapple-ul", "bananas-ul"];
+var subMenuNamesNapis = "";
+
+function takeSubMenuNames() {
+    var subMenuNamesNapis = "";
+    for (i = 0; i < subMenuNames.length; i++) {
+        subMenuNamesNapis = subMenuNamesNapis +" "+ subMenuNames[i];
+    }
+}
 
 function openSubmenuGrapes(x) {
     document.getElementsByClassName("oranges-ul")[0].classList.remove("submenu-opened");
@@ -9,7 +18,6 @@ function openSubmenuGrapes(x) {
     document.getElementsByClassName("bananas-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("apples-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("grapes-ul")[0].classList.toggle("submenu-opened");
-    
 }
 
 function openSubmenuApples(x) {
@@ -19,6 +27,7 @@ function openSubmenuApples(x) {
     document.getElementsByClassName("bananas-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("apples-ul")[0].classList.toggle("submenu-opened");
 }
+
 function openSubmenuOranges(x) {
     document.getElementsByClassName("grapes-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("pineapple-ul")[0].classList.remove("submenu-opened");
@@ -26,6 +35,7 @@ function openSubmenuOranges(x) {
     document.getElementsByClassName("apples-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("oranges-ul")[0].classList.toggle("submenu-opened");
 }
+
 function openSubmenuPineapple(x) {
     document.getElementsByClassName("grapes-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("oranges-ul")[0].classList.remove("submenu-opened");
@@ -33,6 +43,7 @@ function openSubmenuPineapple(x) {
     document.getElementsByClassName("apples-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("pineapple-ul")[0].classList.toggle("submenu-opened");
 }
+
 function openSubmenuBananas(x) {
     document.getElementsByClassName("grapes-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("oranges-ul")[0].classList.remove("submenu-opened");
@@ -40,6 +51,3 @@ function openSubmenuBananas(x) {
     document.getElementsByClassName("apples-ul")[0].classList.remove("submenu-opened");
     document.getElementsByClassName("bananas-ul")[0].classList.toggle("submenu-opened");
 }
-
-
-
